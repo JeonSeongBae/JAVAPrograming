1d2956e158e1a2d98f0c79086b9378b4b46ff385
JeonSeongBae <tjdqo6042@naver.com>
2018-12-12 18:48:29 +0900

    맵 밖으로 이동 방지, 골드획득 또는 몬스터와 만났을 경우 게임종료

--- src/_181121/MiniProject.java
+++ src/_181121/MiniProject.java
@@ -6,14 +6,26 @@ class Main extends Sprite {
 
 	@Override
 	void move(char c) {
-		if (c == 'h') {
+		if (c == 'a') {
 			--x;
-		} else if (c == 'j') {
+		} else if (c == 'w') {
 			--y;
-		} else if (c == 'k') {
+		} else if (c == 's') {
 			++y;
-		} else if (c == 'l') {
+		} else if (c == 'd') {
 			++x;
+		}
+		if (x < 1) {
+			x = 1;
+		}
+		if (x > 17) {
+			x = 17;
+		}
+		if (y < 1) {
+			y = 1;
+		}
+		if (y > 8) {
+			y = 8;
 		}
 	}
 }
@@ -42,6 +54,18 @@ class Monster extends Sprite
 	void move(char c) {
 		x += (Math.random() - 0.5) > 0 ? 1 : -1;
 		y += (Math.random() - 0.5) > 0 ? 1 : -1;
+		if (x < 1) {
+			x = 1;
+		}
+		if (x > 17) {
+			x = 17;
+		}
+		if (y < 1) {
+			y = 1;
+		}
+		if (y > 8) {
+			y = 8;
+		}
 	}
 
 }
@@ -60,39 +84,65 @@ public class MiniProject {
 	public static void main(String[] args) {
 		char[][] map = {
 				{ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
-				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
+				{ '#', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', ',', '#' },
 				{ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }, };
 		sc = new Scanner(System.in);
+
 		Main mine = new Main();
 		Monster monster = new Monster();
 		Gold gold = new Gold();
 
 		while (true) {
+			boolean check = false;
 			for (int y = 0; y < map.length; y++) {
+				boolean check2 = false;
 				for (int x = 0; x < map[y].length; x++) {
+					boolean check3 = false;
+					if (y == mine.y && x == mine.x) {
+						System.out.print('@');
+						check3 = true;
+					}
 					if (y == monster.y && x == monster.x) {
 						System.out.print('M');
-					} else if (y == mine.y && x == mine.x) {
-						System.out.print('@');
+						if (check3) {
+							System.out.println();
+							System.out.println("Fail");
+							check2 = true;
+							break;
+						}
 					} else if (y == gold.y && x == gold.x) {
 						System.out.print('G');
+						if (check3) {
+							System.out.println();
+							System.out.println("Sucess");
+							check2 = true;
+							break;
+						}
 					} else {
 						System.out.print(map[y][x]);
 					}
-					System.out.println();
 				}
-				System.out.print("방향 hjkl");
-				char c = sc.next().charAt(0);
-				mine.move(c);
-				monster.move(c);
+				if (check2) {
+					check = true;
+					break;
+				}
+				System.out.println();
 			}
+			if (check) {
+				System.out.println("게임종료");
+				break;
+			}
+			System.out.print("방향 wsad");
+			char c = sc.next().charAt(0);
+			mine.move(c);
+			monster.move(c);
 		}
 	}
 
